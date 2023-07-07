803b29ea4ce975c1126d7d4d9fe1179a9ed37cdd
Athena Hawkins <Athena.a.hawkins@gmail.coml>
2023-07-07 15:52:42 -0600

    Create filmDAO

--- src/main/java/com/skilldistillery/film/controllers/FilmController.java
+++ src/main/java/com/skilldistillery/film/controllers/FilmController.java
@@ -1,5 +1,11 @@
 package com.skilldistillery.film.controllers;
 
+import org.springframework.stereotype.Controller;
+
+import com.skilldistillery.film.data.FilmDAO;
+
+@Controller
 public class FilmController {
 
+	private FilmDAO filmDao;
 }
